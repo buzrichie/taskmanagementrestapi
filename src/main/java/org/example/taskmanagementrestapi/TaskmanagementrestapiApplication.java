457f5efc082f08626bd4dd5edde8564ef6f55946
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-28 08:37:02 -0700

    Adding Project Start up Structure

---
+++ src/main/java/org/example/taskmanagementrestapi/TaskmanagementrestapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.taskmanagementrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagementrestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/taskmanagementrestapi/TaskmanagementrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taskmanagementrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
