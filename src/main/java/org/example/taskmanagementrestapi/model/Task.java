19fff4d238cfc6d272ed77340f0ef632d4f84f40
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-28 09:18:17 -0700

    feat(model): add Task model Class, it getters and setters and toString method

---
+++ src/main/java/org/example/taskmanagementrestapi/model/Task.java
@@ -0,0 +1,60 @@
+package org.example.taskmanagementrestapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+public class Task {
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String description;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+}
