1f30d1ed43abb23c5cd9925f4c9e6dc9f30df434
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-29 03:34:42 -0700

    feat(service): perf TaskService to use UserDTO for data extraction that will be used to createTask and updatetask

--- src/main/java/org/example/taskmanagementrestapi/service/TaskService.java
+++ src/main/java/org/example/taskmanagementrestapi/service/TaskService.java
@@ -1,6 +1,7 @@
 package org.example.taskmanagementrestapi.service;
 
-import org.example.taskmanagementrestapi.Repository.TaskRepository;
+import org.example.taskmanagementrestapi.repository.TaskRepository;
+import org.example.taskmanagementrestapi.dto.TaskDTO;
 import org.example.taskmanagementrestapi.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,21 +18,28 @@ public class TaskService {
     }
 
     public List<Task> getAllTasks() { return repository.findAll(); }
-    public Task createTask(Task task) { return repository.save(task); }
-    public Task getTask(Long id) { return repository.findById(id).orElse(null); }
-    public Task updateTask(Long id, Task taskDetails) {
-        Task task = repository.findById(id).orElseThrow();
+    public Task createTask(TaskDTO dto) {
+        Task task = new Task();
 
-        task.setTitle(taskDetails.getTitle());
-        task.setDescription(taskDetails.getDescription());
-        task.setStatus(taskDetails.getStatus());
-
+        task.setTitle(dto.getTitle());
+        task.setDescription(dto.getDescription());
+        task.setStatus(dto.getStatus());
+        System.out.println(task);
         return repository.save(task);
     }
+    public Task getTask(Long id) { return repository.findById(id).orElse(null); }
 
-    public void deleteTask(Long id) {
+    public Task updateTask(Long id, TaskDTO dto) {
+        Task task = repository.findById(id).orElseThrow();
+        task.setTitle(dto.getTitle());
+        task.setDescription(dto.getDescription());
+        task.setStatus(dto.getStatus());
+        return repository.save(task);
+    }
+    public Task deleteTask(Long id) {
         Task task = repository.findById(id).orElseThrow();
         repository.deleteById(task.getId());
+        return task;
     }
 }
 
